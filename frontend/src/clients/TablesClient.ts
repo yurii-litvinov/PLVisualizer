d98c894f548782efadb547d07cef15b143614889
oveeernight <rnpozharskiy@gmail.com>
2022-11-16 23:43:59 +0300

    refactor dragdrop region

--- frontend/src/clients/TablesClient.ts
+++ frontend/src/clients/TablesClient.ts
@@ -2,19 +2,21 @@ import axios, {AxiosResponse} from "axios";
 import {Discipline} from "../Models/Discipline";
 import {Lecturer} from "../Models/Lecturer";
 
-export const baseURL = 'http://localhost:3001'
 
 export interface ITablesClient {
-    exportTableAsync: (id : string, lecturers : Lecturer[]) =>  Promise<AxiosResponse>
-    importTableViaLecturersTableAsync: (id: string) =>  Promise<AxiosResponse<Lecturer[]>>
+    exportTableAsync: (spreadsheetId : string, lecturers : Lecturer[]) =>  Promise<AxiosResponse>
+    importTableViaLecturersTableAsync: (spreadsheetId: string) =>  Promise<AxiosResponse<Lecturer[]>>
+    importTableViaConfigAsync: (spreadsheetId: string, file : FormData) => Promise<AxiosResponse<Lecturer[]>>
+    
 }
 
 export const createTablesClient = () : ITablesClient => {
-    let url = `${baseURL}/tables/`
-    const exportTableAsync = (id : string, lecturers : Lecturer[]) => axios.post(`${url}/export/id`, lecturers)
-    const importTableViaLecturersTableAsync = (id: string) => axios.get<Lecturer[]>(`${url}/import/id`)
-    const importLecturersViaConfigAsync = (id: string, FileForm)
+    let url = 'https://localhost:5001/tables'
+    const exportTableAsync = (spreadsheetId : string, lecturers : Lecturer[]) => axios.post(`${url}/export/${spreadsheetId}`, lecturers)
+    const importTableViaLecturersTableAsync = (spreadsheetId: string) => axios.get<Lecturer[]>(`${url}/import/${spreadsheetId}`)
+    const importTableViaConfigAsync = (spreadsheetId: string, file: FormData) => axios.post<Lecturer[]>(`${url}/import/config/${spreadsheetId}`)
     return {exportTableAsync: exportTableAsync,
         importTableViaLecturersTableAsync: importTableViaLecturersTableAsync,
+        importTableViaConfigAsync: importTableViaConfigAsync
     }
 }
